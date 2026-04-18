c0cd7ba84ecd134450a03ea8e46548bf955def45
Dewiyana Vincentia Rinansi <dewiyanavincentiarinansii@gmail.com>
2026-04-18 09:59:39 +0800

    feat: penyiapan blade component untuk layout utama

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Pasien', function () {
+    return view('Pasien.index' , ['title' => 'Pasien']);
+});
+
+Route::get('/Pasien/create', function () {
+    return view('Pasien.create' , ['title' => ' Create Pasien']);
+});
